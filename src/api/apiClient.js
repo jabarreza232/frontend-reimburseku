d4c1de5993b93d658d94e560799d39a59391afac
Muhammad Jabbar <reza.okovian@gmail.com>
2026-07-22 00:18:02 +0700

    change config url api

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,13 +1,21 @@
 import axios from 'axios'
 import { useAuthStore } from '@/stores/auth'
+
+// Jika sedang di-build untuk production (cPanel), arahkan ke domain utama.
+// Jika sedang di lokal (dev), gunakan '/api' agar proxy Vite tetap berjalan.
+const apiUrl = import.meta.env.PROD 
+  ? 'https://reimburseku.my.id/api' 
+  : '/api'
+
 const apiClient = axios.create({
-  baseURL: '/api', // Diproteksi Vite proxy untuk bypass CORS ke server hosted
+  baseURL: apiUrl,
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
   },
   timeout: 10000 // opsional
 })
+
 apiClient.interceptors.request.use((config) => {
   const authStore = useAuthStore()
   const token = authStore.token // Mengambil token dari store
@@ -17,4 +25,5 @@ apiClient.interceptors.request.use((config) => {
   }
   return config
 })
+
 export default apiClient
